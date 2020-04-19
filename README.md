165f9689adcfb40a17e5480a53eef75fb3c4b9e7
Austin Gabe <57731755+thinkblue721@users.noreply.github.com>
2020-04-18 23:54:37 -0700

    Final commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HW3-random-password
-Homework for Week 3 - HTML, CSS, and JS files for random password generator
+HW3-random-password is a random password generating web application. It asks for a set of criteria, including number of characters and types of characters.
+
+# Support
+This application was not finished completely, so any feedback to improve will be welcome.
+
+# Contributing
+Austin was the only one to work on this application.
--- script.js
+++ script.js
@@ -1,71 +1,65 @@
-// <!-- Dynamically updated HTML and CSS powered by js
-// Click the button to generate password and be given series of prompts for password criteria.
-// When prompted for LENGTH, choose length of at least 8 characters and no more than 128.
-// When prompted for chacter types, choose LOWERCASE, UPPERCASE, NUMERIC, and/or SPECIAL CHARACTERS.
-// When each prompt is answered, input should be validated & at least one character type should be selected.
-// When all prompts are answered, then password is generated.
-// Then password is either displayed in ALERT or written to page. -->
-
 // Assignment Code
 var readout = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
+// Arrays for possible character sets
 var lowercaseChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var uppercaseChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numericChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChoices = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "|", "~"];
-//arrays for possible character sets
-
+var finalArray = [""];
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.innerHTML = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
+  // passwordString to put in values of random characters
   var passwordString = "";
+  // Series of prompts requesting criteria
   var passwordLength = parseInt(prompt("Choose length of password between 8 and 128 characters"));
   var useLowercase = confirm("Include lowercase characters in password?");
   var useUppercase = confirm("Include uppercase characters in password?");
   var useNumeric = confirm("Include numbers in password?");
   var useSpecial = confirm("Include special characters in password?");
 
+  // Ensuring that the length of the password is between 8 and 128 characters
   if (passwordLength < 8 || passwordLength > 128 || typeof(passwordLength) !== "number" || passwordLength === NaN || passwordLength === null) {
     alert("Please submit a number between 8 and 128");
     return location.reload;
   }
-
+  // Ensuring that at least one type of chacter is chosen
   else if (useLowercase === false && useUppercase === false && useNumeric === false && useSpecial === false) {
     alert("Please select at least one type of character");
     return location.reload;
   }
-  
+  // Pushing the chosen criteria to one final array
   else {
     if (useLowercase === true) {
-      caseArray.push(lowercaseChoices);
+      finalArray.push(lowercaseChoices);
     }
     if (useUppercase === true) {
-      caseArray.push(uppercaseChoices);
+      finalArray.push(uppercaseChoices);
     }
     if (useNumeric === true) {
-      caseArray.push(numericChoices);
+      finalArray.push(numericChoices);
     }
     if (useSpecial === true) {
-      caseArray.push(specialChoices);
+      finalArray.push(specialChoices);
     }
-
+  // Attempting to randomize from finalArray and add to passwordString using a for loop
     for(var i=0; i<passwordLength; i++) {
-      var password = 
-      var randomArrayChoice = [lowercaseChoices, uppercaseChoices, numericChoices, specialChoices];
-
+       function randomCharacters() {
+         passwordString += finalArray.charAt(Math.floor(Math.random() * passwordLength) + 0);
+       }
+       randomCharacters(i);
     }
-    
+
   }
   
 }
